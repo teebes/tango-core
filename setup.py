607bf801ec450fa34fb9dc17f495dd36477f99bc
Ron DuPlain <ron.duplain@willowtreeapps.com>
2011-01-05 16:43:11 -0500

    Set base project structure, w/namespace packaging.

--- setup.py
+++ setup.py
@@ -1,6 +1,14 @@
-from setuptools import setup
+"""Tango mobile web optimization framework."""
+
+from setuptools import setup, find_packages
 
 setup(
+    name='Tango',
+    version='0.0',
+    description='mobile web optimization framework',
+    long_description=__doc__,
+    packages=find_packages(),
+    namespace_packages=['tango', 'tango.site'],
     install_requires=[
         ],
     tests_require=[
---
+++ tango/__init__.py
@@ -0,0 +1,2 @@
+# Note: This __init__.py should only contain the following line.
+__import__('pkg_resources').declare_namespace(__name__)
