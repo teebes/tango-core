e0aba9a31e11cea9da60fd2ae93f9d9407cb1cfb
Ron DuPlain <ron.duplain@willowtreeapps.com>
2011-01-10 12:00:59 -0500

    Introduce a local custom package for lib tweaks.

---
+++ tango/custom/__init__.py
@@ -0,0 +1,12 @@
+"""Package of customizations of external libraries.
+
+Package, module, class, and function overrides go here as necessary.  Anything
+included here should either tweak an external library's behavior or fix a bug.
+Bugfixes should make their way upstream, as should behavior tweaks if they are
+general enough.  Use of this package allows for development and deployment
+against release versions of external libraries.
+
+Everything in this package follows the namespace of the origin library,
+prefixed by 'tango.custom', e.g. customizations to example.Class end up at
+tango.custom.example.Class.
+"""
