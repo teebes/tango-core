4883ed7829d1e22acda2731ed257f87c08e85c4a
Ron DuPlain <ron.duplain@willowtreeapps.com>
2011-01-10 13:46:41 -0500

    Add version label.

--- tango/manage.py
+++ tango/manage.py
@@ -4,8 +4,15 @@ class Version(object):
     "Version management class for flexibility in project versioning metadata."
     number = '0.0'
     name = ''
+    notice = ''
     maintainer = 'Ron DuPlain'
     maintainer_email = 'ron.duplain@willowtreeapps.com'
+
+    @property
+    def label(self):
+        return ('Tango %(number)s (%(name)s) %(notice)s\n'
+                'Maintainer: %(maintainer)s <%(maintainer_email)s>'
+                % type(self).__dict__)
 
 version = Version()
 
