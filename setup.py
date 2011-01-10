cba4b9e26807759c84fd70b6e040a66b3c800c8d
Ron DuPlain <ron.duplain@willowtreeapps.com>
2011-01-10 15:19:50 -0500

    Start clean Tango history.

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    install_requires=[
+        ],
+    tests_require=[
+        'nose',
+        'minimock',
+        ],
+    test_suite='nose.collector',
+    )
