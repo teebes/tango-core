cd40ec60720b3aabda639860cb334fe6bb62f628
Ron DuPlain <ron.duplain@willowtreeapps.com>
2011-01-11 15:40:08 -0500

    Add prefix function for routes declaration.

--- tango/routes/__init__.py
+++ tango/routes/__init__.py
@@ -1,1 +1,12 @@
 "Package to parse & handle routes declared by a Tango site."
+
+def prefix(head, *paths):
+    """Prefix sequence of strings with given head, for URL route declaration.
+
+    >>> prefix('/figures/numbers/', 'one', 'two', 'three')
+    ('/figures/numbers/one', '/figures/numbers/two', '/figures/numbers/three')
+    >>> prefix('/path/', '', 'to/another')
+    ('/path/', '/path/to/another')
+    >>>
+    """
+    return tuple(head + path for path in paths)
