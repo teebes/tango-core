877b36928d4cc998217865d82c53e4b4bf957c5f
Ron DuPlain <ron.duplain@willowtreeapps.com>
2011-01-11 17:41:07 -0500

    Add create app factory, takes an import name.

--- tango/factory/__init__.py
+++ tango/factory/__init__.py
@@ -1,1 +1,17 @@
 "Package to instantiate a Tango object from a Tango site package."
+
+from ..app import Tango
+
+
+def create_app(import_name):
+    """Create a Tango application object from a Python import name.
+
+    Example, using the default site package in this project:
+    >>> app = create_app('tango.site.default')
+    >>> app.config['SITE']
+    'default'
+    >>>
+    """
+    app = Tango(import_name)
+    app.config.from_object(import_name + '.config')
+    return app
