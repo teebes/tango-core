374c13a32aac924798cb2bfa2442b4aa1025486f
Ron DuPlain <ron.duplain@willowtreeapps.com>
2011-05-17 14:50:48 -0400

    Add tango namespace.

---
+++ tango/__init__.py
@@ -0,0 +1,13 @@
+from flask import abort, request, session
+
+import app
+import build
+import config
+import factory
+import helpers
+import routes
+import tools
+
+
+__all__ = ['abort', 'request', 'session', 'app', 'build', 'config', 'factory',
+           'helpers', 'routes', 'tools']
