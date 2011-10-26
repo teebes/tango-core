5c70e7bdea52e13c19a7b1667c839a4e0a061871
Ron DuPlain <ron.duplain@willowtreeapps.com>
2011-10-26 02:17:06 -0400

    Accept Tango instance or import name in shelve.

--- tango/factory/stash.py
+++ tango/factory/stash.py
@@ -1,11 +1,21 @@
 "Shelve an application's stash."
 
+from tango.app import Tango
 from tango.factory.app import build_app
 
 
-def shelve(import_name, report_file=None):
-    app = build_app(import_name, import_stash=True, use_snapshot=False,
-                    report_file=report_file)
+def shelve(app_or_name, report_file=None):
+    """Shelve the route contexts of an app, given by object or import name.
+
+    Does not return anything, and inherently has side-effects:
+    >>> shelve('simplest')
+    >>> shelve(build_app('simplest'))
+    """
+    if isinstance(app_or_name, Tango):
+        app = app_or_name
+    else:
+        app = build_app(app_or_name, import_stash=True, use_snapshot=False,
+                        report_file=report_file)
     for route in app.routes:
         site, rule, context = route.site, route.rule, route.context
         if report_file is not None:
