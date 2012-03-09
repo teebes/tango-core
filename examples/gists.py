1e173d3e15f9dc692840209fcdf23d685bb42e90
Ron DuPlain <ron.duplain@willowtreeapps.com>
2012-03-08 16:25:03 -0800

    Add gists example.

---
+++ examples/gists.py
@@ -0,0 +1,23 @@
+"""
+site: gists
+routes:
+ - json: /
+exports:
+ - description: A collection of gists from rduplain.
+ - gists
+"""
+
+import requests
+import json
+
+gist_list_url = 'https://api.github.com/users/rduplain/gists'
+gist_detail_url_format = 'https://api.github.com/gists/%s'
+
+gists = []
+for gist_obj in json.loads(requests.get(gist_list_url).text):
+    gist = {}
+    for key in ('description', 'created_at', 'git_pull_url'):
+        gist[key] = gist_obj[key]
+    gist_detail_url = gist_detail_url_format % gist_obj['id']
+    gist['files'] = json.loads(requests.get(gist_detail_url).text)['files']
+    gists.append(gist)
